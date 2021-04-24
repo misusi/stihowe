38c85cc159d3ae39952a954d83d62629faab69a2
Michael Susi <misusi@protonmail.com>
2021-04-23 22:13:59 -0400

    initial commit

---
+++ Assets/Third Person Camera/Scripts/Core/Camera/CameraFollow.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2 : MonoBehaviour {
+
+	public float CameraMoveSpeed = 120.0f;
+	public GameObject CameraFollowObj;
+	Vector3 FollowPOS;
+	public float clampAngle = 80.0f;
+	public float inputSensitivity = 150.0f;
+	public float distance = 1.0f;
+	public float mouseX;
+	public float mouseY;
+	public float finalInputX;
+	public float finalInputZ;
+	public bool invertVertical;
+    public bool invertHorizontal;
+	private float rotY = 0.0f;
+	private float rotX = 0.0f;
+	
+	void Start () {
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotY = rot.y;
+		rotX = rot.x;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	
+	void Update () {
+
+		// float inputX = Input.GetAxis ("RightStickHorizontal");
+		// float inputZ = Input.GetAxis ("RightStickVertical");
+		mouseX = Input.GetAxis ("Mouse X");
+		mouseY = Input.GetAxis ("Mouse Y");
+		finalInputX = /*inputX*/ + mouseX;
+		finalInputZ = /*inputZ*/ + mouseY;
+
+        // Scroll in/out
+        float ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");           //This little peece of code is written by JelleWho https://github.com/jellewie
+        if (ScrollWheelChange != 0)
+        {                                            //If the scrollwheel has changed
+            float R = ScrollWheelChange * 15;                                   //The radius from current camera
+            float PosX = Camera.main.transform.eulerAngles.x + 90;              //Get up and down
+            float PosY = -1 * (Camera.main.transform.eulerAngles.y - 90);       //Get left to right
+            PosX = PosX / 180 * Mathf.PI;                                       //Convert from degrees to radians
+            PosY = PosY / 180 * Mathf.PI;                                       //^
+            float X = R * Mathf.Sin(PosX) * Mathf.Cos(PosY);                    //Calculate new coords
+            float Z = R * Mathf.Sin(PosX) * Mathf.Sin(PosY);                    //^
+            float Y = R * Mathf.Cos(PosX);                                      //^
+            float CamX = Camera.main.transform.position.x;                      //Get current camera postition for the offset
+            float CamY = Camera.main.transform.position.y;                      //^
+            float CamZ = Camera.main.transform.position.z;                      //^
+            Camera.main.transform.position = new Vector3(CamX + X, CamY + Y, CamZ + Z);//Move the main camera
+        }
+
+
+		if (!invertVertical) finalInputZ *= -1;
+		if (invertHorizontal) finalInputX *= -1;
+        
+		rotY += finalInputX * inputSensitivity * Time.deltaTime;
+		rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+		rotX = Mathf.Clamp (rotX, -clampAngle, clampAngle);
+
+		Quaternion localRotation = Quaternion.Euler (rotX, rotY, 0.0f);
+		transform.rotation = localRotation;
+
+
+	}
+
+	void LateUpdate () {
+		CameraUpdater ();
+	}
+
+	void CameraUpdater() {
+		Transform target = CameraFollowObj.transform;
+
+		//move towards the game object that is the target
+		float step = CameraMoveSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+	}
+}
