031aa0c232dcc2a678e1adeccaf52e7acdf682df
Michael Susi <misusi@protonmail.com>
2021-04-29 21:48:06 -0500

    Recentering camera on input.

---
+++ Assets/Scripts/CMCameraRecenter.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using Cinemachine;
+
+public class CMCameraRecenter : MonoBehaviour
+{
+	CinemachineFreeLook m_CMFreeLookCam;
+	float m_RecenterTime;
+	float m_TimeSinceRecenter = 0f;
+	bool m_CurrentlyRecentering = false;
+	private void Start()
+	{
+		m_CMFreeLookCam = GetComponent<CinemachineFreeLook>();
+		// TODO: Is there really no built-in way to do this through
+		// Cinemachine?
+		m_RecenterTime =
+			4 * m_CMFreeLookCam.m_YAxisRecentering.m_RecenteringTime;
+	}
+	void Update()
+	{
+		if (Input.GetButton("RecenterCamera")) StartCentering();
+
+		if (m_CurrentlyRecentering)
+		{
+			m_TimeSinceRecenter += Time.deltaTime;
+			if (m_TimeSinceRecenter >= m_RecenterTime) StopCentering();
+		}
+	}
+
+	void StartCentering()
+	{
+		m_CurrentlyRecentering = true;
+		m_CMFreeLookCam.m_RecenterToTargetHeading.m_enabled = true;
+		m_CMFreeLookCam.m_YAxisRecentering.m_enabled = true;
+		m_CMFreeLookCam.m_RecenterToTargetHeading.RecenterNow();
+		m_CMFreeLookCam.m_YAxisRecentering.RecenterNow();
+	}
+	void StopCentering()
+	{
+
+		m_CMFreeLookCam.m_RecenterToTargetHeading.m_enabled = false;
+		m_CMFreeLookCam.m_YAxisRecentering.m_enabled = false;
+		m_CurrentlyRecentering = false;
+		m_TimeSinceRecenter = 0f;
+	}
+}
